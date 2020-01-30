4c928d79fc395ea5b5a2724bf0c92c9c9850dd75
Matthew Gilliard <matthew@gilliard.lol>
2020-01-30 09:37:35 +0000

    Initial Project Setup
    The project, as downloaded from start.spring.io

---
+++ src/main/java/lol/gilliard/websocketstranscription/WebsocketsTranscriptionApplication.java
@@ -0,0 +1,13 @@
+package lol.gilliard.websocketstranscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketsTranscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketsTranscriptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lol/gilliard/websocketstranscription/WebsocketsTranscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package lol.gilliard.websocketstranscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketsTranscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
